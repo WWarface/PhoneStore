625e17d76e56826caae5780f167109db6c915fa8
Haritone <2003harik2003@gmail.com>
2022-10-22 18:19:34 +0300

    Log Provided

--- PhoneStore/Program.cs
+++ PhoneStore/Program.cs
@@ -1,9 +1,12 @@
+using PhoneStore;
 using PhoneStore.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMvc();
 builder.Services.AddTransient<IEmailSender, EmailSender>();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Logging.AddFile(Path.Combine(Directory.GetCurrentDirectory() + "/Logs/", "logger.txt"));
 var app = builder.Build();
 app.UseStaticFiles();
 
