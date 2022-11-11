85ce84b80b4e5efd1fd14c5f35e9e020d9fe5abf
Haretone Shulge <2003harik2003@gmail.com>
2022-11-11 20:38:43 +0200

    VievComponent

---
+++ PhoneStore/Components/NavBar.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace PhoneStore.Components
+{
+    public class NavBar : ViewComponent
+    {
+        private readonly List<string> _keys;
+        public NavBar()
+        {
+            _keys = new List<string>()
+            {
+                "Home","About","Brand","Specials","Contact"
+            };
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(_keys);
+        }
+    }
+}
